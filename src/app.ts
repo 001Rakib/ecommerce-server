9146f87877d8a33bcc6b019ccd7b9641a4a24f59
001Rakib <rakibrakibrakib354@gmail.com>
2024-05-20 21:07:49 +0600

    created product interface and model

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
-const app = express();
+const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use("/api/products");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
