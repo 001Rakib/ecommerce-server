433a2e3dc9b370c0a51428770ef26af36ff41825
001Rakib <rakibrakibrakib354@gmail.com>
2024-05-21 22:46:30 +0600

    implemented inventory quantity update method after creating a order

--- src/app/modules/orders/order.controller.ts
+++ src/app/modules/orders/order.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import orderValidationSchema from "./order.validation";
 import { orderService } from "./order.service";
+import { Product } from "../products/product.model";
 
 //create order into db
 const createOrder = async (req: Request, res: Response) => {
@@ -9,14 +10,54 @@ const createOrder = async (req: Request, res: Response) => {
     //data validation using Zod
     const zodParsedData = orderValidationSchema.parse(orderData);
 
-    const result = await orderService.createOrderIntoDB(zodParsedData);
+    //checking available quantity of product
+    const productData = await Product.findById(orderData.productId);
+    const availableQuantityBeforeOrder = productData?.inventory
+      .quantity as number;
+    //checking the ordered quantity
+    const orderedQuantity = orderData.quantity;
+    //returning error response if the quantity exceeds the available quantity
+    if (orderedQuantity > availableQuantityBeforeOrder) {
+      res.status(500).json({
+        success: false,
+        message: "Insufficient quantity available in inventory",
+      });
+    } else {
+      const id = productData?._id;
+      //calculating available quantity after order
+      const availableQuantityAfterOrder =
+        availableQuantityBeforeOrder - orderedQuantity;
 
-    //send response
-    res.status(200).json({
-      success: true,
-      message: "Order created successfully!",
-      data: result,
-    });
+      //setting the inStock to false if the available quantity is 0
+      if (availableQuantityAfterOrder === 0) {
+        const updateQuantityAfterOrder = await Product.findByIdAndUpdate(
+          { _id: id },
+          {
+            "inventory.inStock": false,
+          },
+          { new: true }
+        );
+      }
+
+      const updatedQuantityAfterOrder = await Product.findByIdAndUpdate(
+        { _id: id },
+        {
+          "inventory.quantity": availableQuantityAfterOrder,
+        },
+        { new: true }
+      );
+      const result = await orderService.createOrderIntoDB(zodParsedData);
+
+      const remainingQuantity = updatedQuantityAfterOrder?.inventory.quantity;
+      const inStock = updatedQuantityAfterOrder?.inventory.inStock;
+
+      //send response
+      res.status(200).json({
+        success: true,
+        message: "Order created successfully!",
+        data: result,
+      });
+    }
   } catch (err: any) {
     res.status(500).json({
       success: false,
